d1aa84c9044b06a3167afc6317d9e58e9d4d5e37
pangil5634 <oksk6681@naver.com>
2025-08-22 23:37:42 +0900

    chore : image resize, console.log

--- src/components/layout/Introduce.tsx
+++ src/components/layout/Introduce.tsx
@@ -20,11 +20,11 @@ function Introduce({ title, desc, project_id }: { title: string; desc: string, p
         project_id === 1000 ? (
           "" 
         ): (
-          <Link href={`/project/${project_id}`} className="flex mb-3 hover:opacity-50"><Image src="/Img/return.png" className="mr-2" alt="return" width={20} height={20}/>뒤로가기</Link>
+          <Link href={`/project/${project_id}`} className="flex mb-3 hover:opacity-50"><Image src="/Img/return.png" className="mr-2 w-[20px] h-[20px]" alt="return" width={20} height={20}/>뒤로가기</Link>
         )
       }
       <div className="flex items-center">
-        <Image src="/Img/rani.png" alt="logo" className="mr-2" width={24} height={24} />
+        <Image src="/Img/rani.png" alt="logo" className="mr-2 w-[24px] h-[24px]" width={24} height={24} />
         <p className="text-2xl font-bold">{title}</p>
       </div>
       <p className="mt-2 text-xs text-boldGray md:text-base">{formattedDesc}</p>
